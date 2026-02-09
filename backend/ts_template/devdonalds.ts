249cac977e38eb3e199b90f85626264f40b8813b
Anastasia Ayres <z5692718@ad.unsw.edu.au>
2026-02-10 03:19:28 +1100

    Finished task and jsdoc

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 
 // ==== Type Definitions, feel free to add or modify ==========================
+type Cookbook = Record<string, recipe | ingredient>;
+
 interface cookbookEntry {
   name: string;
   type: string;
@@ -25,6 +27,15 @@ interface recipeSummary {
   ingredients: requiredItem[];
 }
 
+enum CookbookType {
+  Recipe = "recipe",
+  Ingredient = "ingredient",
+}
+
+const HyphenUnderscore = /[-_]/g;
+const LetterSpace = /[^a-zA-Z ]/g;
+const MultiSpace = / +/g;
+
 // =============================================================================
 // ==== HTTP Endpoint Stubs ====================================================
 // =============================================================================
@@ -32,7 +43,8 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: Cookbook = {};
+// Given -> const cookbook: any = null;
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -50,16 +62,24 @@ app.post("/parse", (req:Request, res:Response) => {
 
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
+
+/**
+ * Transforms a string to be similar to Title Case with only letters and single 
+ * spacing
+ * 
+ * @param {string} recipeName - String to be transformed
+ * 
+ * @returns {(string | null)} Transformed string
+ */
 const parse_handwriting = (recipeName: string): string | null => {
-  recipeName.replace(/-_/g, ' ');
-  recipeName.replace(/[^a-zA-Z ]/g, '');
-  recipeName.replace(/ +/g, ' ');
-  recipeName.trim();
+  const str = recipeName.replace(HyphenUnderscore, ' ').replace(LetterSpace, '').replace(MultiSpace, ' ').trim();
 
-  if (recipeName.length <= 0) return null;
+  if (str.length <= 0) return null;
 
-  const str = recipeName.split(' ');
-  const recipe = str.map((s) => {s[0].toUpperCase + s.substring(1)}).join(' ');
+  const stringArr = str.split(' ');
+  const recipe = stringArr.map((s) => {
+    return s[0].toUpperCase() + s.substring(1).toLowerCase()}
+  ).join(' ');
 
   return recipe;
 }
@@ -69,46 +89,51 @@ const parse_handwriting = (recipeName: string): string | null => {
 app.post("/entry", (req:Request, res:Response) => {
   const data = req.body;
 
-  if (Object.keys(data).length === 0 || !validateCookbookEntry(data)) {
-    res.status(400);
-  } else {
-    res.status(200);
+  try {
+    validateCookbookEntry(data);
+    res.status(200).json({});
+  } catch (e) {
+    res.status(400).json({});
   }
 });
 
 /**
+ * Validates data makes a unique cookbook entry, and adds to cookbook if true
  * 
- * @param {(recipe | ingredient)} data 
- * @returns 
+ * @param {(recipe | ingredient)} data - Given info to create a new entry
+ * 
+ * @returns {boolean} If the given data makes a valid entry
  */
-function validateCookbookEntry(data: recipe | ingredient): boolean {
+const validateCookbookEntry = (data: recipe | ingredient): void => {
   data.name = parse_handwriting(data.name);
-  if (!data.name) return false;
-  if (cookbook.some(e => e.name === data.name)) return false;
+  if (!data.name || (cookbook[data.name])) {
+    throw new Error('Invalid name or existing entry');
+  }
 
   switch (data.type) {
-    case "recipe":
+    case CookbookType.Recipe:
       const recipe = data as recipe;
-      if (recipe.requiredItems.some((reqItem) => {
-        reqItem.name = parse_handwriting(reqItem.name);
+      
+      const requiredItems = recipe.requiredItems.map(req => parse_handwriting(req.name));
+      const uniqueItems = new Set(requiredItems);
 
-        if (reqItem.name.split(' ').length > 1) return true;
-      })) return false;
+      if (uniqueItems.size !== recipe.requiredItems.length) {
+        throw new Error('Recipe requiredItems can only have one element per name');
+      }
 
-      console.log(recipe.name)
       break;
-    case "ingredient":
+    case CookbookType.Ingredient:
       const ingredient = data as ingredient;
-      if (ingredient.cookTime < 0) return false;
+      if (ingredient.cookTime < 0) {
+        throw new Error('Cook time cannot be less than 0');
+      }
 
       break;
     default:
-      return false;
+      throw new Error ('Type can only be "recipe" or "ingredient".')
   }
 
-  cookbook.push(data);
-
-  return true;
+  cookbook[data.name] = data;
 }
 
 // [TASK 3] ====================================================================
@@ -116,17 +141,26 @@ function validateCookbookEntry(data: recipe | ingredient): boolean {
 app.get("/summary", (req:Request, res:Request) => {
   const name = req.query.name;
 
-  if (!name) return res.status(400)
-
-  const summary = createRecipeSummary(name);
-  if (!summary) return res.status(400)
-
-  res.status(200).json(summary);
+  try {
+    const summary = createRecipeSummary(name);
+    res.status(200).json(summary);
+  } catch (e) {
+    res.status(400).json({err: e.message});
+  }
 });
 
-function createRecipeSummary(name: string): recipeSummary | null {
-  const recipe: recipe = cookbook.find(e => e.name === name);
-  if (recipe.type !== "recipe") return null;
+/**
+ * Creates a summary of the ingredients needed for a given recipe and cooking time
+ * 
+ * @param {string} name - Recipt to create a summary of
+ * 
+ * @returns {recipeSummary} - Summary with ingredients and cooking time
+ */
+const createRecipeSummary = (name: string): recipeSummary => {
+  const recipe = cookbook[name];
+  if (!recipe || recipe.type !== CookbookType.Recipe) {
+    throw new Error('Recipe doesn\' exist or is an ingredient');
+  }
 
   const summary: recipeSummary = {
     name: recipe.name,
@@ -134,26 +168,41 @@ function createRecipeSummary(name: string): recipeSummary | null {
     ingredients: []
   };
 
-  if (addSummary(recipe, 1, summary) === null) return null;
+  addSummary(recipe as recipe, 1, summary);
+  
   return summary;
 }
 
-function addSummary(entry: recipe, numReq: number, sum: recipeSummary): null | void {
+/**
+ * Helper function to create recipe summary. Iterates through required items, 
+ * and recurses when a recipe is found
+ * 
+ * @param {recipe} entry - Cookbook entry to add next to summary
+ * @param {number} numReq - Number of the entry item required
+ * @param {recipeSummary} sum - Summary of recipe
+ */
+const addSummary = (entry: recipe, numReq: number, sum: recipeSummary): void => {
   entry.requiredItems.forEach((item) => {
-    const entry = cookbook.find(e => e.name === item.name);
+    const entry = cookbook[item.name];
 
     if (!entry) {
-      return null;
-    } else if (entry.type === "ingredient") {
-      addIngredientSummary(entry, item.quantity * numReq, sum);
-    } else if (entry.type === "recipe") {
-      addSummary(entry, item.quantity * numReq, sum);
+      throw new Error('Required Item doesn\'t exist in cookbook');
+    } else if (entry.type === CookbookType.Ingredient) {
+      addIngredientSummary(entry as ingredient, item.quantity * numReq, sum);
+    } else if (entry.type === CookbookType.Recipe) {
+      addSummary(entry as recipe, item.quantity * numReq, sum);
     }
   })
 }
 
-
-function addIngredientSummary(entry: ingredient, numReq: number, sum: recipeSummary): void {
+/**
+ * Helper function to add ingredient into summary
+ * 
+ * @param {ingredient} entry - Cookbook ingredient entry to add next to summary
+ * @param {number} numReq - Number of the ingredient item required
+ * @param {recipeSummary} sum - Summary of recipe
+ */
+const addIngredientSummary = (entry: ingredient, numReq: number, sum: recipeSummary): void => {
   sum.cookTime += entry.cookTime * numReq;
 
   const sumEntry = sum.ingredients.find(e => e.name === entry.name);
