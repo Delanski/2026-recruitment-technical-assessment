6b8554fabeb79835f5753fd40ae5aeda92fd49f3
Anastasia Ayres <z5692718@ad.unsw.edu.au>
2026-02-10 01:20:55 +1100

    Implemented tasks - todo style

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -19,6 +19,12 @@ interface ingredient extends cookbookEntry {
   cookTime: number;
 }
 
+interface recipeSummary {
+  name: string;
+  cookTime: number;
+  ingredients: requiredItem[];
+}
+
 // =============================================================================
 // ==== HTTP Endpoint Stubs ====================================================
 // =============================================================================
@@ -45,25 +51,122 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  recipeName.replace(/-_/g, ' ');
+  recipeName.replace(/[^a-zA-Z ]/g, '');
+  recipeName.replace(/ +/g, ' ');
+  recipeName.trim();
+
+  if (recipeName.length <= 0) return null;
+
+  const str = recipeName.split(' ');
+  const recipe = str.map((s) => {s[0].toUpperCase + s.substring(1)}).join(' ');
+
+  return recipe;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const data = req.body;
 
+  if (Object.keys(data).length === 0 || !validateCookbookEntry(data)) {
+    res.status(400);
+  } else {
+    res.status(200);
+  }
 });
+
+/**
+ * 
+ * @param {(recipe | ingredient)} data 
+ * @returns 
+ */
+function validateCookbookEntry(data: recipe | ingredient): boolean {
+  data.name = parse_handwriting(data.name);
+  if (!data.name) return false;
+  if (cookbook.some(e => e.name === data.name)) return false;
+
+  switch (data.type) {
+    case "recipe":
+      const recipe = data as recipe;
+      if (recipe.requiredItems.some((reqItem) => {
+        reqItem.name = parse_handwriting(reqItem.name);
+
+        if (reqItem.name.split(' ').length > 1) return true;
+      })) return false;
+
+      console.log(recipe.name)
+      break;
+    case "ingredient":
+      const ingredient = data as ingredient;
+      if (ingredient.cookTime < 0) return false;
+
+      break;
+    default:
+      return false;
+  }
+
+  cookbook.push(data);
+
+  return true;
+}
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const name = req.query.name;
 
+  if (!name) return res.status(400)
+
+  const summary = createRecipeSummary(name);
+  if (!summary) return res.status(400)
+
+  res.status(200).json(summary);
 });
+
+function createRecipeSummary(name: string): recipeSummary | null {
+  const recipe: recipe = cookbook.find(e => e.name === name);
+  if (recipe.type !== "recipe") return null;
+
+  const summary: recipeSummary = {
+    name: recipe.name,
+    cookTime: 0,
+    ingredients: []
+  };
+
+  if (addSummary(recipe, 1, summary) === null) return null;
+  return summary;
+}
+
+function addSummary(entry: recipe, numReq: number, sum: recipeSummary): null | void {
+  entry.requiredItems.forEach((item) => {
+    const entry = cookbook.find(e => e.name === item.name);
+
+    if (!entry) {
+      return null;
+    } else if (entry.type === "ingredient") {
+      addIngredientSummary(entry, item.quantity * numReq, sum);
+    } else if (entry.type === "recipe") {
+      addSummary(entry, item.quantity * numReq, sum);
+    }
+  })
+}
+
+
+function addIngredientSummary(entry: ingredient, numReq: number, sum: recipeSummary): void {
+  sum.cookTime += entry.cookTime * numReq;
+
+  const sumEntry = sum.ingredients.find(e => e.name === entry.name);
+
+  if (sumEntry) {
+    sumEntry.quantity += numReq;
+  } else {
+    sum.ingredients.push({
+      name: entry.name,
+      quantity: numReq,
+    })
+  }
+};
 
 // =============================================================================
 // ==== DO NOT TOUCH ===========================================================
