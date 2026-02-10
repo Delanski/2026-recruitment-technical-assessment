70295edddcf610434dc17b874f3f617e72533035
Anastasia Ayres <z5692718@ad.unsw.edu.au>
2026-02-10 15:01:12 +1100

    Question 1 - rust, done

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -3,9 +3,23 @@ use serde::{Deserialize, Serialize};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
+    let mut string_len: usize = 0;
+    let mut int_sum: i32 = 0;
+
+    for item in request.data {
+        match item {
+            DataType::String(s) => {
+                string_len += s.len();
+            },
+            DataType::Num(n) => {
+                int_sum += n;
+            }
+        }
+    }
 
     let response = DataResponse {
-        
+        string_len: string_len,
+        int_sum: int_sum
     };
 
     (StatusCode::OK, Json(response))
@@ -13,10 +27,18 @@ pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse
 
 #[derive(Deserialize)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<DataType>
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: usize,
+    int_sum: i32,
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum DataType {
+    String(String),
+    Num(i32),
 }
